1a227d0338b38d8e3f17ee72f3e98022947bb36e
Idris Yakub <olayinkayakub@yahoo.com>
2022-05-17 04:29:58 +0100

    nailed it

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,18 +1,42 @@
+import { toBeDisabled } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
+import { useState } from 'react/cjs/react.production.min';
 
 function Slides({slides}) {
+    const [active, setActive] = React.useState(0)
+    const allslides = slides;
 
+    const reset = ()=>{
+        return(setActive(0));
+    }
+    const next = ()=>{
+        let click = active
+        return(setActive(++click));
+    }
+    const previous = ()=>{
+        let click = active
+        return(setActive(--click));
+    }
+    const slideContent=()=>{
+        let data = allslides[active]
+        let{title, text} = data
+        return(
+            <>
+                <div id="slide" className="card text-center">
+                    <h1 data-testid="title">{title}</h1>
+                    <p data-testid="text">{text}</p>
+                </div>
+            </>
+        );
+    }
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={()=>reset()} disabled={active===0}>Restart</button>
+                <button data-testid="button-prev" className="small" onClick={()=>previous()} disabled={active===0}>Prev</button>
+                <button data-testid="button-next" className="small" onClick={()=>next()} disabled={active=== allslides.length - 1}>Next</button>
             </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
+            {slideContent()}
         </div>
     );
 
